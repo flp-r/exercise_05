8bfe4a4f750dc3bd89337affdcd449104bae5320
Katerina Sabatova <sabatovak@vut.cz>
2024-09-24 10:28:13 +0200

    Reformat template for Hirchberg function

--- hirschberg_template.R
+++ hirschberg_template.R
@@ -2,77 +2,67 @@
 #' 
 #' @param X DNAString object representing NT or AA sequence to be aligned.
 #' @param Y DNAString object representing NT or AA sequence to be aligned.
-#' @param align A list of DNAString objects with alignment of input sequences.
+#' @param alignment A list of DNAString objects with alignment of input sequences.
 #' @param match An integer value of a score for matching bases.
 #' @param mismatch An integer value of a score for mismatching bases.
 #' @param gap An integer value of a penalty for gap insertion.
 #' @returns A list of DNAString objects with alignment of input sequences.
-HirschbergTemplate <- function(X, Y, align, match, mismatch, gap){
+HirschbergTemplate <- function(X, Y, alignment, match, mismatch, gap){
     
-    first_align_row <- align[[1]] # initialize the first row of alignment
-    second_align_row <- align[[2]] # initialize the second row of alignment
+    first_alignment_row <- alignment[[1]] # initialize the first row of alignment
+    second_alignment_row <- alignment[[2]] # initialize the second row of alignment
   
-  
-    if # length of X is equal to zero
-    {
-        for # for each character in Y
-        {
-            first_align_row <- # add gap
-            second_align_row <- # add character from Y
+    if () { # length of X is equal to zero
+        for () { # for each character in Y
+            first_alignment_row <- # add gap
+            second_alignment_row <- # add character from Y
         }
-        align <- c(DNAStringSet(first_align_row), DNAStringSet(second_align_row))
-        print(align)
-    }
-    else if # length of Y is equal to zero
-    {
-        for # for each character in X
-        {
-            first_align_row <- # add character from X
-            second_align_row <- # add gap
+        alignment <- c(DNAStringSet(first_alignment_row), DNAStringSet(second_alignment_row))
+        print(alignment)
+    } else if () { # length of Y is equal to zero
+        for () { # for each character in X
+            first_alignment_row <- # add character from X
+            second_alignment_row <- # add gap
         }
-        align <- c(DNAStringSet(first_align_row), DNAStringSet(second_align_row))
-        print(align)
-    }
-    else if # length of X and Y is equal to 1
-    {
-        first_align_row <- # add character from X
-        second_align_row <- # add character from Y
-        align <- c(DNAStringSet(first_align_row), DNAStringSet(second_align_row))
-        print(align)
-    }
-    else
-    {
+        alignment <- c(DNAStringSet(first_alignment_row), DNAStringSet(second_alignment_row))
+        print(alignment)
+    } else if () { # length of X and Y is equal to 1
+        first_alignment_row <- # add character from X
+        second_alignment_row <- # add character from Y
+        alignment <- c(DNAStringSet(first_alignment_row), DNAStringSet(second_alignment_row))
+        print(alignment)
+    } else {
         xlen <- # length of X
         xmid <- # half of the length of X
         ylen <- # length of Y
-        
-        left_score <- # NW score for the first half of X and the whole Y
-        right_score <- # NW score for the second half of X and the whole Y (both are reversed)
+
+        # NW score for the first half of X and the whole Y
+        first_score <-
+        # NW score for the second half of X and the whole Y (both are reversed)
+        second_score <-
+
         ymid <- # index of division for Y
 
         # The first half
-        if # index of division for Y is equal to 0
-        {
-            align <- # call Hirschberg function for the first half of X and for an empty DNAString object
-        }
-        else
-        {
-            align <- # call Hirschberg function for the first half of X and for the first part of Y
+        if () { # index of division for Y is equal to 0
+            # call Hirschberg function for the first half of X and for an empty DNAString object
+            alignment <-
+        } else {
+            # call Hirschberg function for the first half of X and for the first part of Y
+            alignment <-
         }
         
         # The second half
-        if ((xmid + 1) > xlen) # X cannot be further divided
-        {
-            align <- # call Hirschberg function for an empty DNAString object and the second half of Y
-        }
-        else if ((ymid + 1) > ylen) # Y cannot be further divided
-        {
-            align <- # call Hirschberg function for the second half of X and for an empty DNAString object
-        }
-        else 
-        {
-            align <- # call hirschberg function for the second half of X and the second part of Y
+        if ((xmid + 1) > xlen) { # X cannot be further divided
+            # call Hirschberg function for an empty DNAString object and the second half of Y
+            alignment <-
+        } else if ((ymid + 1) > ylen) { # Y cannot be further divided
+            # call Hirschberg function for the second half of X and for an empty DNAString object
+            alignment <-
+        } else {
+            # call Hirschberg function for the second half of X and the second part of Y
+            alignment <-
         }
     }
-    return(align)
+    return(alignment)
 }
